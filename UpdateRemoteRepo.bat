1c71ee7290edd6fc04941db47cde021b0d564b98
shann2112 <l00177543@atu.ie>
2022-11-11 09:38:55 +0000

    try 5

--- UpdateRemoteRepo.bat
+++ UpdateRemoteRepo.bat
@@ -3,37 +3,27 @@
 # Date: 11/11/2022
 # Function: Perform a commit
 # Script: UpdateRemoteRepo.bat
+
 clear
 @echo off
-git status
 echo.
 echo '**************************************************'
 echo "Performing an add for all files in this directory"
 echo '**************************************************'
 echo.
-
 git status
-
 echo *** press [ctrl][c] to exit or any key to continue ***
 pause
-
-:: set variables to local
-
-SETLOCAL
 echo.
 set /p NAME=Enter the name of the project, then press [return]
 
-#git commit -m "%NAME%"
-#git status
 echo '**************************************************'
 echo 'Pushing to GITHUB repository'
 
-#git branch  -m main
 git remote add origin https://github.com/shann2112/Assignment1
 git add .
 git commit -m "%NAME%"
 git push origin main
-
 
 echo '**************************************************'
 echo 'Done!
